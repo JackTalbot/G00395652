80b2f10c706acb514414c800c23eb315764ad3ee
JackTalbot <81542947+JackTalbot@users.noreply.github.com>
2022-05-07 00:32:56 +0100

    thing

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
+import { IonicModule, NavController } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HomePageRoutingModule } from './home-routing.module';
+
+NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    HomePageRoutingModule
+  ],
+  declarations: [HomePage]})
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
 
-  constructor() {}
-
-}
+export class HomePage {}
