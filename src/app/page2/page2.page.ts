2c9ba8e58f2148231b0630d06b1a29efc067b03f
JackTalbot <81542947+JackTalbot@users.noreply.github.com>
2022-05-07 14:02:40 +0100

    things

--- src/app/page2/page2.page.ts
+++ src/app/page2/page2.page.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page2.page.scss'],
 })
 export class Page2Page implements OnInit {
-
+myStatus: string = "";
   constructor() { }
 
   ngOnInit() {
